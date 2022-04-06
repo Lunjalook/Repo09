03ad0b5cd88faa194a25ec8bf9c121949c327655
Lunjalook <azra.cosickic@gmail.com>
2022-04-06 07:34:17 +0200

    Adding exercise 17 - while loops

---
+++ ex17/src/index.js
@@ -0,0 +1,21 @@
+var fiveNumbers = '';
+
+function inverseWhile() {
+    var i = 5;
+    var j = 5;
+
+    while (i >= 0) {
+
+        fiveNumbers += i;
+        if (j !== 0) {
+            fiveNumbers += ',';
+            j--;
+        }
+        i--;
+    }
+    return fiveNumbers;
+}
+
+
+console.log(inverseWhile());
+module.exports = inverseWhile;
